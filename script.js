8d0faef58c6b80b131a5783f978d4984bf53625f
helvizar <helvizaranderson@gmail.com>
2023-09-22 21:44:44 +0700

    feat: implemented toast notifications for form input validation

--- script.js
+++ script.js
@@ -11,14 +11,36 @@ function isNumber(str) {
 
 // Fungsi untuk memvalidasi data formulir
 function validateFormData(data) {
-  if (
-    data &&
-    isNumber(data.zipCode) &&
-    data.status === true
-  ) {
-    return true;
+  if (!data) {
+    return false;
   }
-  return false;
+
+  if (data.name.trim() === '') {
+    showToast('Nama harus diisi.');
+    return false;
+  }
+
+  if (data.city.trim() === '') {
+    showToast('Kota harus diisi.');
+    return false;
+  }
+
+  if (data.email.trim() === '') {
+    showToast('Email harus diisi.');
+    return false;
+  }
+
+  if (!isNumber(data.zipCode)) {
+    showToast('Kode Pos harus berisi angka.');
+    return false;
+  }
+
+  if (!data.status) {
+    showToast('Anda harus menyetujui pernyataan.');
+    return false;
+  }
+
+  return true;
 }
 
 // Fungsi untuk mendapatkan data dari formulir
@@ -34,26 +56,16 @@ function handleGetFormData() {
   const email = emailInput.value;
   const zipCode = zipCodeInput.value;
 
-  // Memeriksa apakah checkbox dicentang
-  const isCheckboxChecked = checkboxIsChecked();
-
-  // Validasi zip-code untuk memastikan hanya angka yang diterima
-  if (!isNumber(zipCode)) {
-    warningDiv.textContent = 'Kode Pos harus berisi angka.';
-    return; // Berhenti dan tidak mengirimkan formulir jika kode pos tidak valid
-  }
-
   // Validasi seluruh data formulir menggunakan fungsi validateFormData
   const formData = {
     name,
     city,
     email,
     zipCode,
-    status: isCheckboxChecked,
+    status: checkboxIsChecked(),
   };
 
   if (!validateFormData(formData)) {
-    warningDiv.textContent = 'Periksa form anda sekali lagi.';
     return; // Berhenti dan tidak mengirimkan formulir jika validasi gagal
   }
 
@@ -61,6 +73,7 @@ function handleGetFormData() {
   warningDiv.textContent = '';
 
   // Proses formulir (kirim atau simpan data, dll.) dilakukan di sini
+  showToast('Formulir berhasil dikirim!'); // Menampilkan toast sukses
 }
 
 // Fungsi untuk menangani submit form
@@ -72,3 +85,28 @@ function submit(event) {
 // Menghubungkan event submit form dengan fungsi submit
 const form = document.getElementById('register-form');
 form.addEventListener('submit', submit);
+
+// Menambahkan class "hide" pada elemen toast saat halaman dimuat
+document.addEventListener('DOMContentLoaded', () => {
+  const toast = document.getElementById('toast');
+  toast.classList.add('hide');
+});
+
+// Fungsi untuk menampilkan toast
+function showToast(message) {
+  const toast = document.getElementById('toast');
+  const toastContent = document.getElementById('toast-content');
+
+  // Set the message text
+  toastContent.textContent = message;
+
+  // Show the toast with animation
+  toast.classList.remove('hide');
+  toast.classList.add('show');
+
+  // Hide the toast after a few seconds with animation
+  setTimeout(() => {
+    toast.classList.remove('show');
+    toast.classList.add('hide');
+  }, 3000); // Adjust the duration (in milliseconds) as needed
+}
